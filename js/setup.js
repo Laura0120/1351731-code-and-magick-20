793bc29476d98a5648e47c8864a89e6a5d6237fa
a_galustyan <a_galustyan@head-point.ru>
2020-06-07 15:23:38 +0300

    правки по замечаниям 2

--- js/setup.js
+++ js/setup.js
@@ -11,9 +11,10 @@ var COAT_COLOR = [
   ' rgb(0, 0, 0)',
 ];
 var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARDS_COUNT = 4;
 
+var wizards = [];
 var userDialog = document.querySelector('.setup');
-
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
@@ -22,15 +23,15 @@ var getRandomItem = function (array) {
 };
 
 var createWizards = function () {
-  var wizards = [];
-  for (var i = 0; i < 4; i++) {
-    wizards.push({
+  var generatedWizards = [];
+  for (var i = 0; i < WIZARDS_COUNT; i++) {
+    generatedWizards.push({
       name: getRandomItem(NAME) + ' ' + getRandomItem(SURNAME),
       coatColor: getRandomItem(COAT_COLOR),
       eyesColor: getRandomItem(EYES_COLOR),
     });
   }
-  return wizards;
+  return generatedWizards;
 };
 
 var createWizardElement = function (wizard) {
@@ -42,8 +43,6 @@ var createWizardElement = function (wizard) {
 };
 
 var renderWizardElements = function () {
-  var wizards = createWizards();
-
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < wizards.length; i++) {
     fragment.appendChild(createWizardElement(wizards[i]));
@@ -57,5 +56,6 @@ var showSimilarWizards = function () {
   userDialog.querySelector('.setup-similar').classList.remove('hidden');
 };
 
+wizards = createWizards();
 renderWizardElements();
 showSimilarWizards();
